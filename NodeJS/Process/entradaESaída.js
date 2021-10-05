97b164f53c50ead4b7ae4fe2aaab32944a77a0ce
Marcus-vinicius-pcs <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2021-10-05 11:52:27 -0300

    process

--- NodeJS/Process/entradaESaída.js
+++ NodeJS/Process/entradaESaída.js
@@ -2,4 +2,12 @@ const anonimo = process.argv.indexOf('-a') !== -1
 
 if(anonimo){
     process.stdout.write('Fala anonimo!\n')
+    process.exit()
+} else {
+    process.stdout.write('Digite seu nome: ')
+    process.stdin.on('data', data => {
+        const nome = data.toString().replace('\n', '')
+        process.stdout.write(`Ola ${nome}!\n`)
+        process.exit()
+    })
 }
